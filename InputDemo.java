2033decc26bce90b8ca925bdfcb111c06f74c875
Shrikrishnan2018 <100992791+Shrikrishnan2018@users.noreply.github.com>
2022-03-17 22:26:35 +0530

    Add files via upload

---
+++ InputDemo.java
@@ -0,0 +1,13 @@
+package org.inputfromuser;
+
+public class InputDemo {
+	public static void main(String[] args) {
+		/* kfdsjfkdjffsdkjdskllmvlkjjmlkcoijnczkmczknjjdfkjnsdfkdsbfkjsndfkjnsfdnfnfjnsfnsfjnff
+		 * fndskjfkjsdfndsnf;jdsnfjnfdsfnsfdlndkmds
+		 * lfjsndkfndffnsd
+		 * sdnfdfkjnsdfsdmm*/
+		 int ticketPrice = 800;
+		 System.out.println("\"helloworld\"");
+	}
+
+}
